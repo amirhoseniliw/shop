d9e9861d785dba1acd4e59bbfea23162b28e90df
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2025-03-15 21:19:59 +0330

    update header

--- application/view/app/layouts/header.php
+++ application/view/app/layouts/header.php
@@ -320,7 +320,7 @@
     <span class="top-header-user-panel-text">پنل کاربری</span>  
 </a> 
                             <?php }?>
-                        <a class="top-header-cart btn" data-bs-toggle="offcanvas" href="" role="button" aria-controls="offcanvasCart">
+                        <a class="top-header-cart btn" data-bs-toggle="offcanvas" href="ثقثث" role="button" aria-controls="offcanvasCart">
                         <span class="top-header-cart-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M10.591 8.42484H12.6669C12.9816 8.42484 13.2289 8.16461 13.2289 7.8508C13.2289 7.52934 12.9816 7.27677 12.6669 7.27677H10.591C10.2762 7.27677 10.0289 7.52934 10.0289 7.8508C10.0289 8.16461 10.2762 8.42484 10.591 8.42484ZM15.1324 4.44562C15.5896 4.44562 15.8893 4.60635 16.1891 4.95843C16.4889 5.3105 16.5413 5.81565 16.4739 6.27412L15.7619 11.295C15.627 12.2602 14.8177 12.9712 13.8659 12.9712H5.68979C4.69307 12.9712 3.86871 12.1913 3.78627 11.181L3.09681 2.83755L1.96519 2.63855C1.66543 2.58498 1.45559 2.28648 1.50805 1.98032C1.56051 1.66728 1.85278 1.45987 2.16004 1.50655L3.9474 1.78133C4.2022 1.82801 4.38955 2.04156 4.41204 2.30179L4.55443 4.01624C4.57691 4.26193 4.77176 4.44562 5.01157 4.44562H15.1324ZM5.56973 14.1809C4.94023 14.1809 4.43062 14.7014 4.43062 15.3443C4.43062 15.9795 4.94023 16.5 5.56973 16.5C6.19175 16.5 6.70135 15.9795 6.70135 15.3443C6.70135 14.7014 6.19175 14.1809 5.56973 14.1809ZM14.0007 14.1809C13.3712 14.1809 12.8616 14.7014 12.8616 15.3443C12.8616 15.9795 13.3712 16.5 14.0007 16.5C14.6227 16.5 15.1323 15.9795 15.1323 15.3443C15.1323 14.7014 14.6227 14.1809 14.0007 14.1809Z" fill="#0E1935"></path>
@@ -358,19 +358,19 @@
                                 </ul>
                             </li>
                             <li class="nav-item">
-                                <a href="" class="nav-link border-animate f-600 fromCenter">
-                                    <i class="bi bi-tag"></i>تخفیف ها و پیشنهاد ها
+                                <a href="<?php $this->url('/product/index/cheap') ?>" class="nav-link border-animate f-600 fromCenter">
+                                    <i class="bi bi-grid"></i>دیدن کل محصولات  
                                 </a>
                             </li>
                             <li class="nav-item"><a href="" class="nav-link f-600 border-animate fromCenter">
-                                <i class="bi bi-question-octagon"></i>
-                                سوالی
-                                دارید</a>
+                                <i class="bi bi-chat"></i>
+                                ارتباط با ما 
+                                </a>
                             </li>
                             <li class="nav-item"><a href="" class="nav-link f-600 border-animate fromCenter">
-                                <i class="bi bi-bag-heart"></i>
-                                در استادینو
-                                بفروشید</a></li>
+                                <i class="bi bi-info-circle"></i>
+                                 درباره ما 
+                                </a></li>
                         </ul>
                     </div>
                 </div>
